dea7a70a26e511e11361262aab9e1384d3578ec0
Eric <zu19890223@gmail.com>
2022-04-16 15:44:13 +0800

    api

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,5 +1,5 @@
 // import { $env } from '@/env'
-import axios from 'axios'
+import axios from "axios";
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
@@ -9,23 +9,23 @@ import axios from 'axios'
 const config = {
   baseURL:
     // process.env.apiBaseURL || $env.apiBaseUrl || 'http://localhost:3008/',
-    process.env.APIBASEURL || 'http://54.225.110.131:80/',
+    process.env.APIBASEURL || "http://api.orbiter.finance"
 
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
-}
+};
 
-const $axios = axios.create(config)
+const $axios = axios.create(config);
 
 $axios.interceptors.request.use(
   function (config) {
     // Do something before request is sent
-    return config
+    return config;
   },
   function (error) {
     // Do something with request error
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export { $axios }
+export { $axios };
